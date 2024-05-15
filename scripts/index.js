c2511b1bc8b843ffe94d60690007a7a875472c3c
victor santos <victor.santos@lexartlabs.xyz>
2024-05-15 12:25:23 -0300

    feat: Made css for pc and fixed bugs in js

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,16 @@ const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 
 let pos = 0;
-const itemMaxWidth = 300;
+const itemMaxWidth = 248;
 // console.log(pos, "position");
 
 // Função para mover o carrossel
 function moveCarousel() {
-  // alert(`${pos} POSITION. ${carousel.children.length} length`)
+  // const containerWidth = document.querySelector('.carousel-container').offsetWidth;
+  // const itemsPerPage = Math.floor(containerWidth / itemMaxWidth);
+  // const itemsCount = carousel.children.length;
+  // const maxPosition = (itemsCount - itemsPerPage) * itemMaxWidth;
+  // alert(`${pos} POSITION. ${carousel.children.length} length; ${maxPosition} maxPosition`)
   carousel.style.transform = `translateX(-${pos}px)`;
 }
 
@@ -17,25 +21,25 @@ nextBtn.addEventListener('click', () => {
   const containerWidth = document.querySelector('.carousel-container').offsetWidth;
   const itemsPerPage = Math.floor(containerWidth / itemMaxWidth);
   const itemsCount = carousel.children.length;
-  const maxPosition = (itemsCount - itemsPerPage) * itemMaxWidth - itemMaxWidth;
+  const maxPosition = (itemsCount - itemsPerPage) * itemMaxWidth;
 
-  if (pos < maxPosition) {
+  if (pos <= maxPosition) {
       pos += itemMaxWidth;
       moveCarousel();
   }
-  if (pos >= maxPosition) {
-    pos = itemMaxWidth;
+  if (pos > maxPosition) {
+    pos = 0;
     moveCarousel();
   }
 });
 
 // Event listener para o botão de retroceder
 prevBtn.addEventListener('click', () => {
-  if (pos > 0) {
+  if (pos >= 0) {
       pos -= itemMaxWidth;
       moveCarousel();
   }
-  if (pos === 0) {
+  if (pos < 0) {
     pos = (itemMaxWidth * carousel.children.length) - (itemMaxWidth * 5);
     moveCarousel();
   }
@@ -46,13 +50,16 @@ function populateCarousel(products) {
   products.forEach((product, idx) => {
     const item = document.createElement('div');
     item.classList.add('carousel-item');
+    if (idx === 0) item.style.marginLeft = `1px`;
     item.innerHTML = `
-      <span style="width: 240px; height: 300px; display: flex; justify-content: center; align-content: center;">
-        <img class="carousel_prod_img" loading="lazy" width="240" height="300" src="${product.image}" alt="${product.title}">
+      <span class="span_carousel_img" style="width: 205px; height: 256.25px; display: flex; justify-content: center; align-content: center;">
+        <img class="carousel_prod_img" loading="lazy" src="${product.image}" alt="${product.title}">
       </span>
-      <h3>${product.title}</h3>
-      <p class="carousel_prod_descr">${product.description}</p>
-      <strong>S/ $${product.price}</strong>
+      <div style="width: 205px; height: 138px">
+        <h3 style="height: 20px; overflow: hidden;">${product.title}</h3>
+        <p class="carousel_prod_descr">${product.description}</p>
+        <strong style="font-size: 19px; font-weight: 700;">S/ ${Number(product.price).toFixed(2)}</strong>
+      </div>
     `;
     carousel.appendChild(item);
   });
